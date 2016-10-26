01e8bdaccb428e46954f41bf05183af288817bf9
zheng39562 <zheng39562@163.com>
2016-10-26 11:03:59 -0400

    增加登录的makefile和基本接口文件（都未实现）

---
+++ src/login/login_interface.h
@@ -0,0 +1,23 @@
+/**********************************************************
+ *  \!file login_interface.h
+ *  \!brief
+ *  \!note	注意事项： 
+ * 			1,类中的成员函数中的同名参数的含义完全相同。仅会注释其中一个函数，其他函数则不再重复注释。重名的参数意义不同时，会独立注解。 
+ * 			2,第1条的规则同样适用于返回值的含义。 
+ * 
+ * \!version 
+ * * \!author zheng39562@163.com
+**********************************************************/
+#ifndef _login_interface_H
+#define _login_interface_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
+
