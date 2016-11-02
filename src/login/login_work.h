c05ac0b3e151a6b17f9c8034b4033e12e0919019
zheng39562 <zheng39562@163.com>
2016-11-02 11:01:28 -0400

    调整文档的位置。

--- src/login/login_work.h
+++ src/login/login_work.h
@@ -1,5 +1,5 @@
 /**********************************************************
- *  \!file login_impl.h
+ *  \!file login_work.h
  *  \!brief
  *  \!note	注意事项： 
  * 			1,类中的成员函数中的同名参数的含义完全相同。仅会注释其中一个函数，其他函数则不再重复注释。重名的参数意义不同时，会独立注解。 
@@ -8,28 +8,25 @@
  * \!version 
  * * \!author zheng39562@163.com
 **********************************************************/
-#ifndef _login_impl_H
-#define _login_impl_H
+#ifndef _login_work_H
+#define _login_work_H
 
 #include "fr_public/pub_define.h"
+#include "fr_public/pub_thread.h"
 
 namespace FrLogin{
 	//! \brief	
-	//! \note	工作流程见work。登录器使用短连接（使用后马上断掉）
-	//! \note	使用线程池。
-	//! \note	登录服务器瞬间承压可能不会太好。但暂时没办法分担压力。
-	class LoginImpl{
-		public:
-			LoginImpl();
-			~LoginImpl();
-		public:
-			//! \brief	监听端口
-			void listen(const string &ip, unsigned int port);
-			void close();
-		private:
-			;
+	//! \note	工作流程：
+	//				* 监听登录端口
+	//				* 客户端连接请求。
+	//				* 成功后，直接通过线程查询和验证帐号密码。
+	//				* 验证成功后，打包客户端需要的信息发送给客户端。
+	//				* 发送成功后，登录服务器主动断开链接。
+	//				* 后续需要客户端主动去连接对应的网关服务器。
+	class LoginWorker : public FCThread{
+
 	};
-	typedef 
 }
+
 #endif 
 
